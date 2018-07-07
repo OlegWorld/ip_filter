ae6327c22bb9469d5387767e1577f0e568e14783
Oleg Mironov <osmironov@gmail.com>
2018-07-07 18:57:16 +0300

    sort ok

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <tuple>
 
 // ("",  '.') -> [""]
 // ("11", '.') -> ["11"]
@@ -10,54 +12,53 @@
 // ("11.", '.') -> ["11", ""]
 // (".11", '.') -> ["", "11"]
 // ("11.22", '.') -> ["11", "22"]
-std::vector<std::string> split(const std::string& str, char d)
+std::vector<uint8_t > split(const std::string& str, char d)
 {
-    std::vector<std::string> r;
+    std::vector<uint8_t> r;
+    r.reserve(4);
 
     std::string::size_type start = 0;
     std::string::size_type stop = str.find_first_of(d);
     while(stop != std::string::npos)
     {
-        r.push_back(str.substr(start, stop - start));
+        r.emplace_back(stoul(str.substr(start, stop - start)));
 
         start = stop + 1;
         stop = str.find_first_of(d, start);
     }
 
-    r.push_back(str.substr(start));
+    r.emplace_back(stoul(str.substr(start)));
 
     return r;
-}
-
-void get_ip(std::string& str)
-{
-    str.resize(str.find_first_of('\t'));
 }
 
 int main(int argc, char const *argv[])
 {
     try
     {
-        std::vector<std::vector<std::string> > ip_pool;
+        std::vector<std::vector<uint8_t>> ip_pool;
 
         for(std::string line; std::getline(std::cin, line);)
         {
-            std::vector<std::string> v = split(line, '\t');
-            ip_pool.push_back(split(v.at(0), '.'));
+            //std::vector<std::string> v = split(line, '\t');
+            line.resize(line.find_first_of('\t'));
+
+            ip_pool.emplace_back(split(line, '.'));
         }
 
         // TODO reverse lexicographically sort
+        std::sort(ip_pool.begin(), ip_pool.end(), [](const auto& lhs, const auto& rhs) {
+            return std::tie(lhs[0], lhs[1], lhs[2], lhs[3]) >= std::tie(rhs[0], rhs[1], rhs[2], rhs[3]);
+        });
 
-        for(std::vector<std::vector<std::string> >::const_iterator ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
+        for(const auto& ip : ip_pool)
         {
-            for(std::vector<std::string>::const_iterator ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part)
+            for(const auto& ip_part : ip)
             {
-                if (ip_part != ip->cbegin())
-                {
-                    std::cout << ".";
+                std::cout << unsigned(ip_part);
 
-                }
-                std::cout << *ip_part;
+                if (ip_part != ip.back())
+                    std::cout << ".";
             }
             std::cout << std::endl;
         }
